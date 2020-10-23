a1b71704a3fc9511c13439eedde8c9f616657445
thiegomoura <thi-ego@hotmail.com>
2020-10-23 16:45:49 -0400

    initial commit gym-manager

---
+++ gym-manager/routes.js
@@ -0,0 +1,22 @@
+const { request } = require('express')
+const express = require('express')
+const routes = express.Router()
+const instructors = require('./instructors')
+
+routes.get('/', function (request, response) {
+    return response.redirect("instructors")
+})
+routes.get('/instructors', function (request, response) {
+    return response.render("instructors/index")
+})
+
+routes.get('/instructors/create', function (request, response) {
+    return response.render('instructors/create')
+})
+
+routes.post('/instructors', instructors.post)
+routes.get('/members', function (request, response) {
+    return response.render("members")
+})
+
+module.exports = routes
--- init-backend/data.js
+++ init-backend/data.js
@@ -3,36 +3,42 @@ module.exports = [
         id: "tC8xGPY6v6c",
         title: "MASTER YI VS MASTER YI NO URF KKKK",
         duration: "57 min",
-        price: "R$ 0,00"
+        price: "R$ 0,00",
+        featured: true
     },
     {
         id: "tC8xGPY6v6c",
         title: "MASTER YI VS MASTER YI NO URF KKKK",
         duration: "57 min",
-        price: "R$ 0,00"
+        price: "R$ 0,00",
+        featured: true
     },
     {
         id: "tC8xGPY6v6c",
         title: "MASTER YI VS MASTER YI NO URF KKKK",
         duration: "57 min",
-        price: "R$ 0,00"
+        price: "R$ 0,00",
+        featured: false
     },
     {
         id: "tC8xGPY6v6c",
         title: "MASTER YI VS MASTER YI NO URF KKKK",
         duration: "57 min",
-        price: "R$ 0,00"
+        price: "R$ 0,00",
+        featured: false
     },
     {
         id: "tC8xGPY6v6c",
         title: "MASTER YI VS MASTER YI NO URF KKKK",
         duration: "57 min",
-        price: "R$ 0,00"
+        price: "R$ 0,00",
+        featured: false
     },
     {
         id: "tC8xGPY6v6c",
         title: "MASTER YI VS MASTER YI NO URF KKKK",
         duration: "57 min",
-        price: "R$ 0,00"
+        price: "R$ 0,00",
+        featured: false
     },
 ]
